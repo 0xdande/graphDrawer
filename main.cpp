1af0bc6cba788d2d28604fff9e5bd58b189bcd90
dande <dande@lads.aletheia.icu>
2021-02-10 18:00:21 +0200

    deleted excessive data

--- main.cpp
+++ main.cpp
@@ -15,35 +15,6 @@ int main(int argc, char* argv[]) {
   engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
   if (engine.rootObjects().isEmpty()) return -1;
   auto a = engine.findChild<QObject*>("window")->findChild<QObject*>("graph");
-  //  qDebug() << a->property("width");
   Logic logic;
-  //  QMetaObject::invokeMethod(logic.canvas, "drawCircle", Q_ARG(int, 10),
-  //                            Q_ARG(int, 10));
-  /*qDebug() << engine.rootObjects();
-  QQmlComponent component(&engine, QUrl(QStringLiteral("qrc:/canvas.qml")));
-  if (component.isError()) {
-    qWarning() << "QmlItemCreator::createQmlObject The QMLComponent for "
-               << " has errors: " << component.errorString();
-  } else if (component.isReady()) {
-    QObject* object = component.create();
-    QQuickItem* item = qobject_cast<QQuickItem*>(object);
-
-    object->setParent(engine.rootObjects()[0]);
-    item->setParentItem(qobject_cast<QQuickItem*>(engine.rootObjects()[0]));
-    object->setProperty("parent",
-                        QVariant::fromValue<QObject*>(engine.rootObjects()[0]));
-    qDebug() << object->parent();
-    object->setProperty("width", object->parent()->property("width"));
-    object->setProperty("height", object->parent()->property("height"));
-    //                            Q_ARG(int, 100));
-    qDebug() << object->property("width");
-    qDebug() << object->property("height");
-    QMetaObject::invokeMethod(object, "requestPaint");
-    QMetaObject::invokeMethod(object, "requestPaint");
-    QMetaObject::invokeMethod(object, "requestPaint");
-  }
-  //  logic.engine_ = engine;
-  //  qDebug() << engine;
-  */
   return app.exec();
 }
